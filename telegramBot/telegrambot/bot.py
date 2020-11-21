0435a50968ff91a8edab898a5d0234a2434acbd0
aNOOBis666 <mr.zooko@mail.ru>
2020-11-21 21:10:00 +0300

    BugFix

--- telegramBot/telegrambot/bot.py
+++ telegramBot/telegrambot/bot.py
@@ -1,6 +1,7 @@
 import telebot
 from telegrambot.database.db import Database
 from telegrambot.database.models import Teacher, Student
+from sqlalchemy.exc import IntegrityError
 
 access_token = "1489218543:AAGKxQNnAjHPLyc3LTYlk4adYWZKEj91NXE"
 bot = telebot.TeleBot(access_token)
@@ -12,6 +13,16 @@ db = Database('sqlite:///university.db')
 def handle_start(message):
     user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
     user_markup.row('/registration')
+    bot.send_message(message.from_user.id,
+                     "Добро пожаловать, перед началом работы,\n"
+                     "Пожалуйста, ознакомьтесь с функционалом.\n"
+                     "Прежде всего вам необходимо зарегистрироваться",
+                     reply_markup=user_markup)
+
+
+@bot.message_handler(commands=['help'])
+def handle_start(message):
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
     user_markup.row('/write_message')
     user_markup.row('/main_quest', '/deadline_check')
     bot.send_message(message.from_user.id,
@@ -25,11 +36,8 @@ def handle_start(message):
 def first_try(message):
     if message.text == '/registration':
         send = bot.send_message(message.chat.id, "Введите данные в следующем формате:\n"
-                                                 "Иванович (фамилия)\n"
-                                                 "Иван (имя)\n"
-                                                 "Иванов (отчество)\n"
-                                                 "Математика (предмет)")
-        bot.register_next_step_handler(send, registration)
+                                                 "Преподаватель / Студент ")
+        bot.register_next_step_handler(send, go_next)
     elif message.text == '/write_message':
         send = bot.send_message(message.chat.id, "Введите данные в следующем формате:\n"
                                                  "K1234 K3412 (группы)\n"
@@ -37,20 +45,31 @@ def first_try(message):
         bot.register_next_step_handler(send, message_to_groups)
 
 
-def registration(message):
+def registration(status, message):
     try:
-        status, surname, name, patronymic, subject = message.text.split('\n')
+        surname, name, patronymic, subject = message.text.split('\n')
+        if (status == 'преподаватель') or (status == 'Преподаватель'):
+            status = 0
+        elif (status == 'студент') or (status == 'Студент'):
+            status = 1
+        else:
+            bot.send_message(message.chat.id, 'Проверьте правильность введённых данных')
     except ValueError:
         bot.send_message(message.chat.id, 'Некорректные данные, повторите попытку')
         message.text = '/registration'
         first_try(message)
         return
-    if status == 0:
-        db.add(Teacher(message.chat.id, name, surname, patronymic, subject))
-    else:
-        db.add(Student(message.chat.id, name, surname, patronymic, subject))
-    bot.send_message(message.chat.id, "{name} {patronymic}, Вы успешно зарегестрированы!".format(name=name,
-                                                                                                 patronymic=patronymic))
+    try:
+        if status == 0:
+            db.add(Teacher(message.chat.id, name, surname, patronymic, subject))
+        else:
+            db.add(Student(message.chat.id, name, surname, patronymic, subject))
+    except IntegrityError:
+        return bot.send_message(message.chat.id, "Вы уже зарегистрированы!\n Нажмите /help, чтобы продолжить ")
+    bot.send_message(message.chat.id,
+                     "{name} {patronymic}, Вы успешно зарегестрированы!\n Нажмите /help, чтобы продолжить".format(
+                         name=name,
+                         patronymic=patronymic))
 
 
 def message_to_groups(message):
@@ -66,6 +85,24 @@ def message_to_groups(message):
             if student.group == group:
                 bot.send_message(student.user_id, mess)
     bot.send_message(message.chat.id, "Сообщения отправлены!")
+
+
+def go_next(message):
+    if (message.text == 'преподаватель') or (message.text == 'Преподаватель'):
+        sent = bot.send_message(message.chat.id, "Иванов (фамилия)\n"
+                                                 "Иван (имя) \n"
+                                                 "Иванович (отчество)\n"
+                                                 "Математика (предмет)")
+        bot.register_next_step_handler(sent, registration)
+
+    elif (message.text == 'студент') or (message.text == 'Студент'):
+        sent = bot.send_message(message.chat.id, "Иванович (фамилия)\n"
+                                                 "Иван (имя) \n"
+                                                 "Иванов (отчество)\n"
+                                                 "k3240 (Учебная группа)")
+        bot.register_next_step_handler(sent, registration)
+    else:
+        bot.send_message(message.text.id, 'Проверьте правильность введённых данных')
 
 
 if __name__ == '__main__':
